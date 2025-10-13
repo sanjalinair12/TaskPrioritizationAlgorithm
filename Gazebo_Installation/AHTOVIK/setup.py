fc80a7dae98770d83277827585c8419d6fc6914b
sanjalinair12 <anjali.santhosh@graduate.univaq.it>
2025-10-13 10:13:07 +0200

    Update setup.py

--- Gazebo_Installation/AHTOVIK/setup.py
+++ Gazebo_Installation/AHTOVIK/setup.py
@@ -1,28 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
-package_name = 'AHTOVIK'
+package_name = 'robot_allocator'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/spawn_robots.launch.py', 'launch/allocator_node.launch.py']),
-        ('share/' + package_name + '/urdf', ['urdf/drone.urdf.xacro', 'urdf/ground_robot.urdf.xacro', 'urdf/creeper_robot.urdf.xacro']),
-        ('share/' + package_name, ['robot_criteria_populated.xlsx']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xacro')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='AHTOVIK',
+    maintainer='Anjali',
     maintainer_email='sanjaliasn@gmail.com',
-    description='Heterogeneous Multi-robot task allocation with Gazebo integration',
-    license='Apache-2.0',
+    description='Heterogeneous Robot team formation and Task allocation package using AHP–TOPSIS–VIKOR hybrid logic.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'allocator_node = AHTOVIK.allocator_node:main'
+            'allocator = robot_allocator.allocator_node:main',  # must match function name
         ],
     },
 )
